8f08002ec7911c31d0c2d5b76b9c0b1369e3c82e
Alexander Guryanov <caiiiycuk@gmail.com>
2022-12-07 18:00:06 +0300

    move logging.h to jsdos-log.h

--- include/logging.h
+++ include/logging.h
@@ -18,40 +18,7 @@
 
 #ifndef DOSBOX_LOGGING_H
 #define DOSBOX_LOGGING_H
-enum LOG_TYPES {
-	LOG_ALL = 0,
-	LOG_VGA, LOG_VGAGFX,LOG_VGAMISC,LOG_INT10,
-	LOG_SB,LOG_DMACONTROL,
-	LOG_FPU,LOG_CPU,LOG_PAGING,
-	LOG_FCB,LOG_FILES,LOG_IOCTL,LOG_EXEC,LOG_DOSMISC,
-	LOG_PIT,LOG_KEYBOARD,LOG_PIC,
-	LOG_MOUSE,LOG_BIOS,LOG_GUI,LOG_MISC,
-	LOG_IO,
-	LOG_PCI,
-	LOG_MSG,
-        LOG_NET,
-        LOG_MAX,
-};
 
-enum LOG_SEVERITIES {
-	LOG_NORMAL = 0,
-	LOG_WARN,
-	LOG_ERROR,
-	LOG_SEVERITY_MAX,
-};
-
-class Logger {
-private:
-	LOG_TYPES       d_type;
-	LOG_SEVERITIES  d_severity;
-public:
-	Logger(LOG_TYPES type, LOG_SEVERITIES severity);
-	void operator() (char const* format, ...);
-};
-
-Logger& getLogger(LOG_TYPES type, LOG_SEVERITIES severity);
-#define LOG(type,severity) getLogger(type,severity)
-#define LOG_MSG(format,...)  getLogger(LOG_MSG, LOG_NORMAL)(format, ##__VA_ARGS__)
-#define LOG_ERR(format,...)  getLogger(LOG_MSG, LOG_ERROR)(format, ##__VA_ARGS__)
+#include <jsdos-log.h>
 
 #endif //DOSBOX_LOGGING_H
